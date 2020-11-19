33b8e25100fc674376a6a6abf85f41ed788d2246
Irving Cabello <irvollo@gmail.com>
2020-11-18 20:19:25 -0600

    Add base and underlying contracts

--- app/contracts/Underlying/index.js
+++ app/contracts/Underlying/index.js
@@ -1,9 +1,13 @@
 import DAI from './DAI.json';
 import USDC from './USDC.json';
 import USDT from './USDT.json';
+import WBTC from './WBTC.json';
+import WETH from './WETH.json';
 
 export {
     DAI,
     USDC, 
-    USDT
+    USDT,
+    WBTC,
+    WETH, 
 }
