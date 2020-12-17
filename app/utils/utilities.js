9e08a0647346ce468b55078b755cfcf1a2f98b79
Irving Cabello <irvollo@gmail.com>
2020-12-17 16:14:58 -0600

    Fix decimals with units and coins method

--- app/utils/utilities.js
+++ app/utils/utilities.js
@@ -62,23 +62,49 @@ function numberToString(num)
   return numStr;
 }
 
-export function numberToBN(value_number, decimals) {
+function valid(amount, decimals) {
+  const regex = new RegExp(`^\\d+${decimals > 0 ? `(\\.\\d{1,${decimals}})?` : ''}$`);
+  return regex.test(amount);
+}
 
+function coins(units, decimals) {
+  if (!valid(units, 0)) throw new Error('Invalid amount');
+  if (decimals == 0) return units;
+  const s = units.padStart(1 + decimals, '0');
+  return s.slice(0, -decimals) + '.' + s.slice(-decimals);
+}
+
+function units(coins, decimals) {
+  if (!valid(coins, decimals)) throw new Error('Invalid amount');
+  let i = coins.indexOf('.');
+  if (i < 0) i = coins.length;
+  const s = coins.slice(i + 1);
+  return coins.slice(0, i) + s + '0'.repeat(decimals - s.length);
+}
+
+
+export function numberToBN(value_number, decimals) {
+  let parsedNumber = value_number.toString();
   if (decimals === 1e18) {
-    return numberToString(value_number * decimals)
-  } else {
-    return (value_number * decimals).toString();
+    return units(parsedNumber, 18)
+  }
+  if (decimals === 1e8) {
+    return units(parsedNumber, 8)
+  }
+  if (decimals === 1e6) {
+    return units(parsedNumber, 6)
   }
 }
 
 export function BNtoNumber(value_number, decimals) {
+  let parsedNumber = value_number.toString();
   if (decimals === 1e18) {
-    return RoundNum(value_number / decimals, 18)
+    return coins(parsedNumber, 18)
   }
   if (decimals === 1e8) {
-    return RoundNum(value_number / decimals, 8)
+    return coins(parsedNumber, 8)
   }
   if (decimals === 1e6) {
-    return RoundNum(value_number / decimals, 6)
+    return coins(parsedNumber, 6)
   }
 }
